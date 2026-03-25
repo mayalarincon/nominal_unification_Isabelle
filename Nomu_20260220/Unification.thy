8306609d10eeeabd6a83fd9072295ab9045b26c7
Guilherme <borgesbrandao13@gmail.com>
2026-03-24 21:41:59 -0300

    Auxiliary lemmas

--- Nomu_20260220/Unification.thy
+++ Nomu_20260220/Unification.thy
@@ -224,12 +224,192 @@ next
    using all_solutions_def U_equ_symm by simp
 qed
 
-
 (* the only stuck problems are the "failed" problems and the empty problem *)
+
+lemma red_plus_first_step:
+  assumes "P \<Turnstile> (nabla,s) \<Rightarrow> P'"
+  shows
+    "(\<exists>s1 P2. P \<turnstile> s1 \<leadsto> P2) \<or>
+     (\<exists>nabla1 P2. P \<turnstile> nabla1 \<rightarrow> P2)"
+  using assms by (cases rule: red_plus.cases) blast+
+
+lemma c_red_empty_eqs:
+  assumes "P \<turnstile> nabla \<rightarrow> P'"
+  shows "fst P = []"
+  using assms by (cases rule: c_red.cases) auto
+
+lemma c_red_not_nonempty_eqs:
+  shows "\<not> ((t1 \<approx>? t2) # xs, ys) \<turnstile> nabla \<rightarrow> P'"
+proof
+  assume assm: "((t1 \<approx>? t2) # xs, ys) \<turnstile> nabla \<rightarrow> P'"
+  then show False
+    using c_red_empty_eqs[OF assm]
+      fst_conv[of \<open>(t1 \<approx>? t2) # xs\<close> ys] list.distinct[of \<open>(t1 \<approx>? t2)\<close> xs]
+    by simp
+qed
+
+lemma sred_exists_if_same_shape:
+  assumes "(t1 = Paar t11 t12 \<and> t2 = Paar t21 t22) \<or>
+     ( t1 = Func F t1' \<and> t2 = Func F t2') \<or>
+     ( t1 = Abst a t1' \<and> t2 = Abst a t2') \<or>
+     ( t1 = Abst a t1' \<and> t2 = Abst b t2' \<and> (a\<noteq>b)) \<or>
+     ( t1 = Atom a \<and> t2 = Atom a) \<or>
+     ( t1 = Susp pi1 X \<and> t2 = Susp pi2 X) \<or>
+     (t1 = Susp pi X \<and> t2 = t \<and> (\<not> occurs X t)) \<or>
+     ( t1 = t \<and> t2 = Susp pi X \<and> (\<not> occurs X t)) \<or> (t1 = Unit \<and> t2 = Unit)"
+  shows "\<exists> s P'. ((t1 \<approx>? t2) # xs, ys) \<turnstile> s \<leadsto> P'"
+  using assms
+  apply auto
+proof-
+  {assume "t1 = Abst a t1'" "t2 = Abst b t2'" "(a\<noteq>b)"
+  then have "((Abst a t1' \<approx>? Abst a t2')#xs,ys) \<turnstile>[]\<leadsto> ((t1'\<approx>?t2')#xs,ys)"
+    using abst_ab_sred by auto
+  thus "\<exists>s aa ba. ((Abst a t1', Abst b t2') # xs, ys) \<turnstile> s  \<leadsto> (aa, ba)"
+    by blast}
+
+  {assume "t1 = Susp pi X" "\<not> occurs X t" "t2 = t"
+  then have "((Susp pi X\<approx>?t)#xs,ys) 
+                               \<turnstile>[(X,swap (rev pi) t)]\<leadsto> apply_subst [(X,swap (rev pi) t)] (xs,ys)"
+    using var_1_sred by simp
+  thus "\<exists>s a b. ((Susp pi X, t) # xs, ys) \<turnstile> s \<leadsto> (a, b)" 
+    using prod.collapse by metis}
+
+  {assume "t2 = Susp pi X" "\<not> occurs X t" "t1 = t"
+  then have "((t\<approx>?Susp pi X)#xs,ys) 
+                               \<turnstile>[(X,swap (rev pi) t)]\<leadsto> apply_subst [(X,swap (rev pi) t)] (xs,ys)"
+    using var_2_sred by simp
+  thus "\<exists>s a b. ((t, Susp pi X) # xs, ys) \<turnstile> s \<leadsto> (a, b)" 
+    using prod.collapse by metis}
+qed
+
+lemma red_plus_exists_if_same_shape:
+  assumes
+    "(t1 = Paar t11 t12 \<and> t2 = Paar t21 t22) \<or>
+     (t1 = Func F t1' \<and> t2 = Func F t2') \<or>
+     (t1 = Abst a t1' \<and> t2 = Abst a t2') \<or>
+     (t1 = Abst a t1' \<and> t2 = Abst b t2' \<and> (a\<noteq>b)) \<or>
+     (t1 = Atom a \<and> t2 = Atom a) \<or>
+     (t1 = Susp pi1 X \<and> t2 = Susp pi2 X) \<or>
+     (t1 = Susp pi X \<and> t2 = t \<and> (\<not> occurs X t)) \<or>
+     (t1 = t \<and> t2 = Susp pi X \<and> (\<not> occurs X t)) \<or>
+     (t1 = Unit \<and> t2 = Unit)"
+  shows "\<exists>nabla s P'. ((t1 \<approx>? t2) # xs, ys) \<Turnstile> (nabla,s) \<Rightarrow> P'"
+using assms
+proof -
+  have "\<exists> s P2. ((t1 \<approx>? t2) # xs, ys) \<turnstile> s \<leadsto> P2"
+    using sred_exists_if_same_shape[OF assms] by simp
+  then obtain s P2 where
+  step: "((t1 \<approx>? t2) # xs, ys) \<turnstile> s \<leadsto> P2"
+    by auto
+  then have "((t1 \<approx>? t2) # xs, ys) \<Turnstile> ({}, s) \<Rightarrow> P2"
+    using sred_single by simp
+  then show ?thesis by blast
+qed
+
+
+lemma not_reduce_then_fail:
+  assumes "\<not> (\<exists>nabla s P'. ((t1 \<approx>? t2) # xs, ys) \<Turnstile> (nabla,s) \<Rightarrow> P')"
+  shows "fail ((t1 \<approx>? t2) # xs, ys)"
+  using assms
+proof-
+  have no_shape:
+  "\<not> ((t1 = Paar t11 t12 \<and> t2 = Paar t21 t22) \<or>
+      (t1 = Func F t1' \<and> t2 = Func F t2') \<or>
+      (t1 = Abst a t1' \<and> t2 = Abst a t2') \<or>
+      (t1 = Abst a t1' \<and> t2 = Abst b t2' \<and> a \<noteq> b) \<or>
+      (t1 = Atom a \<and> t2 = Atom a) \<or>
+      (t1 = Susp pi1 X \<and> t2 = Susp pi2 X) \<or>
+      (t1 = Susp pi X \<and> t2 = t \<and> \<not> occurs X t) \<or>
+      (t1 = t \<and> t2 = Susp pi X \<and> \<not> occurs X t) \<or>
+      (t1 = Unit \<and> t2 = Unit))"
+  for t11 t12 t21 t22 F t1' t2' a b pi1 pi2 pi X t
+    using assms red_plus_exists_if_same_shape by blast 
+
+
+lemma fresh_reduces_if_not_atom:
+  assumes "t \<noteq> Atom a"
+  shows "\<exists>P2 nabla s. ([], (a \<sharp>? t) # xs) \<Turnstile> (nabla,s) \<Rightarrow> P2"
+  using assms cred_single
+proof(cases t)
+  case (Abst b t')
+  then show "\<exists>P2 nabla s. ([], (a \<sharp>? t) # xs) \<Turnstile> (nabla, s) \<Rightarrow> P2"
+  proof(cases "a=b")
+    case True
+    hence "([], (a \<sharp>? t) # xs) \<turnstile>{}\<rightarrow> ([],xs)"
+      unfolding Abst using abst_aa_cred by simp
+    then show "\<exists>P2 nabla s. ([], (a \<sharp>? t) # xs) \<Turnstile> (nabla, s) \<Rightarrow> P2"
+      using cred_single by blast
+  next
+    case False
+    hence "([], (a \<sharp>? t) # xs) \<turnstile>{}\<rightarrow> ([], (a\<sharp>? t') # xs)"
+      unfolding  Abst using abst_ab_cred by simp
+    then show "\<exists>P2 nabla s. ([], (a \<sharp>? t) # xs) \<Turnstile> (nabla, s) \<Rightarrow> P2"
+      using cred_single by blast
+  qed
+next
+  case (Susp pi X)
+  then show "\<exists>P2 nabla s. ([], (a \<sharp>? t) # xs) \<Turnstile> (nabla, s) \<Rightarrow> P2"
+    using cred_single by blast
+next
+  case Unit
+  then show "\<exists>P2 nabla s. ([], (a \<sharp>? t) # xs) \<Turnstile> (nabla, s) \<Rightarrow> P2"
+    using cred_single by blast
+next
+  case (Atom b)
+  with assms
+  have "a \<noteq> b" by simp
+  hence "([], (a \<sharp>? t) # xs) \<turnstile> {}\<rightarrow> ([],xs)"
+    unfolding Atom using atom_cred by simp
+  then show "\<exists>P2 nabla s. ([], (a \<sharp>? t) # xs) \<Turnstile> (nabla, s) \<Rightarrow> P2"
+    using cred_single by blast
+next
+  case (Paar t1 t2)
+  then show "\<exists>P2 nabla s. ([], (a \<sharp>? t) # xs) \<Turnstile> (nabla, s) \<Rightarrow> P2"
+    using cred_single by blast
+next
+  case (Func f t')
+  then show "\<exists>P2 nabla s. ([], (a \<sharp>? t) # xs) \<Turnstile> (nabla, s) \<Rightarrow> P2"
+    using cred_single by blast
+qed
+
 
 lemma stuck_equiv: 
   shows "stuck = {([],[])} \<union> {P1. fail P1}"      
-  sorry
+proof (rule set_eqI, rule iffI)
+  fix P
+  {assume P_is_stuck: "P \<in> stuck"
+  then obtain eqs freshs where 
+    P_def: "P = (eqs, freshs)" by (cases P)
+  show "P \<in> {([], [])} \<union> {P1. fail P1}"
+  proof(cases eqs)
+    case Nil
+    then show "P \<in> {([], [])} \<union> {P1. fail P1}"
+    proof(cases freshs)
+      case Nil
+      with \<open>eqs = []\<close>
+      show "P \<in> {([], [])} \<union> {P1. fail P1}" using P_def by simp
+    next
+      case (Cons c freshs')
+      then obtain a t where c_def: "c = a \<sharp>? t" by force
+      have "t = Atom a" 
+        using fresh_reduces_if_not_atom P_is_stuck 
+        unfolding stuck_def P_def Nil Cons c_def by blast
+      hence "fail P" 
+        unfolding P_def Nil Cons c_def using fail_fresh_atom by simp
+      thus "P \<in> {([], [])} \<union> {P1. fail P1}" by auto
+    qed
+  next
+    case(Cons e eqs')
+    then obtain s t where e_def: "e = s \<approx>? t" by force
+    have "fail P" 
+      using P_is_stuck unfolding P_def Cons e_def 
+        stuck_def using not_reduce_then_fail by simp
+    thus "P \<in> {([], [])} \<union> {P1. fail P1}" by auto
+  qed }
+
+  {assume "P \<in> {([], [])} \<union> {P1. fail P1}"
+    show "P \<in> stuck" sorry}
+qed
 
 
 lemma u_empty_sred: 
@@ -255,30 +435,7 @@ lemma empty_then_fail:
 assumes "U P1={}"
 shows" (\<forall>P \<in> results P1. fail P)"
   using assms
-proof-
-
-apply(simp add: results_def)
-  apply(rule conjI)
-  apply(rule impI)
-  apply(simp add: stuck_equiv)
-  apply(erule disjE)
-   apply(subgoal_tac "({},[])\<in>U ([],[])")
-
-apply(simp)
-apply(simp add: all_solutions_def)
-apply(assumption)
-apply(rule impI)+
-apply(rule allI)+
-apply(rule impI)
-apply(erule conjE)
-apply(simp add: stuck_equiv)
-apply(auto)
-apply(subgoal_tac "({},[])\<in>U ([],[])")
-apply(simp add: P1_from_P2_red_plus)
-apply(simp add: ext_subst_def)
-apply(auto)
-apply(simp add: all_solutions_def)
-done*)
+  sorry
 
 (* if a problem can be solved then no "failed" problem is produced *)
 
